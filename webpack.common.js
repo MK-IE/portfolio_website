8ff4a43e399e604a2f3394300decb1a7b5ba5c1f
Marcin Krzykowski <marcin.krzykowski.2018@mumail.ie>
2020-09-10 23:27:23 +0100

    small few changes
    added a little of webpack got the svgs to be loaded directly from html. Adding currently about me section and finishing this website.

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,22 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: ["babel-polyfill", "./src/javascript/index.js"],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/,
+                loader: "file-loader",
+                options: {
+                    name: "[name].[hash].[ext]",
+                    outputPath: "css/images",
+                },
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
