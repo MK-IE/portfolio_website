75f3efad4a902e04ea94652196dac2aa9e92be49
InferNovac <maricn.krzykowski.2018@mumail.ie>
2020-12-15 15:03:32 +0000

    Revamped design again

--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,23 +1,9 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import ProjectGallery from "./components/ProjectsGallery";
-
-const projects = [
-    {
-        title: "First Card",
-        subHeading: "Lorem",
-    },
-    { title: "Second Card", subHeading: "Lorem" },
-    { title: "Third Card", subHeading: "Lorem" },
-    { title: "Fourth Card", subHeading: "Lorem" },
-    { title: "Fifth Card", subHeading: "Lorem" },
-    { title: "Sixth Card", subHeading: "Lorem" },
-];
+import MainPage from "./components/MainPage";
 
 const App = () => (
     <div>
-        <NavBar />
-        <ProjectGallery projects={projects} />
+        <MainPage />
     </div>
 );
 
---
+++ src/javascript/components/MainPage.js
@@ -0,0 +1,31 @@
+const MainPage = () => (
+    <article>
+        <header>
+            <h1>
+                Marcin <span className="main-highlight">Krzykowski</span>
+            </h1>
+            <h2>Code goes brr... 💻</h2>
+        </header>
+        <main>
+            <section className="icons-space">
+                <a href="#" className="icons" title="DrawingTogether">
+                    <i class="fas fa-paint-brush"></i>
+                </a>
+                <a href="#" className="icons" title="Foodber">
+                    <i class="fas fa-pizza-slice"></i>
+                </a>
+                <a href="#" className="icons" title="ProjectNet">
+                    <i class="fas fa-chart-line"></i>
+                </a>
+                <a href="#" className="icons" title="BurgerBuilder">
+                    <i class="fas fa-hamburger"></i>
+                </a>
+                <a href="#" className="icons" title="EmojiDraw">
+                    <i class="fas fa-icons"></i>
+                </a>
+            </section>
+        </main>
+    </article>
+);
+
+export default MainPage;
