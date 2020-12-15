05d40901d728749ff28ca290ef8660ec45332b4a
InferNovac <maricn.krzykowski.2018@mumail.ie>
2020-12-15 13:02:33 +0000

    Small fixes

--- src/javascript/components/ProjectsGallery.js
+++ src/javascript/components/ProjectsGallery.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ProjectGallery = ({ projects }) => {
+    const [position, updatePosition] = useState(0);
+
     const PROJECT_LIST = [];
 
-    for (const project of projects) {
+    PROJECT_LIST.push(
+        <button
+            className="project-btn"
+            onClick={() => updatePosition(position - 100)}
+        >
+            LEFT
+        </button>
+    );
+
+    for (let project = 0; project < projects.length; project++) {
         PROJECT_LIST.push(
             <div key={project} className="project">
-                <h1 className="project-title">{project.title}</h1>
-                <h2 className="mt-1">{project.subHeading}</h2>
+                <h1 className="project-title">{projects[project].title}</h1>
+                <h2 className="mt-1">{projects[project].subHeading}</h2>
             </div>
         );
     }
 
-    return <div className="mt-6 project-container">{PROJECT_LIST}</div>;
+    PROJECT_LIST.push(
+        <button
+            className="project-btn-right"
+            onClick={() => updatePosition(position + 100)}
+        >
+            RIGHT
+        </button>
+    );
+
+    return (
+        <div
+            className="mt-6 project-container"
+            style={{ transform: `translateX(${position}px)` }}
+        >
+            {PROJECT_LIST}
+        </div>
+    );
 };
 
 export default ProjectGallery;
