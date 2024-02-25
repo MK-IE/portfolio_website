1ed66106afa2342c3d8d7b94b571c42d01b7f576
MK-IE <45517444+MK-IE@users.noreply.github.com>
2024-02-25 19:49:46 +0000

    New portfolio page using github pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './javascript/App';
-import './css/font.css';
-import './css/style.css';
-import './css/icons.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
